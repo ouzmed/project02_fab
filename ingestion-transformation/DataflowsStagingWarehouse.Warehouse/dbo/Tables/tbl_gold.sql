bc1b67a2163c9dcace9c7bd90b89e1902318c468
ouzmed <148863961+ouzmed@users.noreply.github.com>
2024-09-17 09:35:06 +0200

    Committing 6 items from workspace 3fd97ea8-3580-4931-853f-80a7e902eae5

---
+++ ingestion-transformation/DataflowsStagingWarehouse.Warehouse/dbo/Tables/tbl_gold.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[tbl_gold] (
+
+	[CustomerID] varchar(8000) NULL, 
+	[CompanyName] varchar(8000) NULL, 
+	[City] varchar(8000) NULL, 
+	[Region] varchar(8000) NULL, 
+	[Country] varchar(8000) NULL
+);
+
